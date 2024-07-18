5536d560c31b27c594d3bb3988ac2e70688be99d
Matias-07 <matias.ferrari.m@gmail.com>
2024-07-18 01:30:57 -0300

    day 1

---
+++ dia 1/EuclidianDistance.py
@@ -0,0 +1,3 @@
+from math import sqrt
+
+print(sqrt((10-2)**2+(8-3)**2));
---
+++ dia 1/Helloworld.py
@@ -0,0 +1,27 @@
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+#mypersonalInformation
+
+print(type("Matias"))
+print(type("Ferrari - Mazzeo"))
+print(type("Argentina"))
+print(type("I am enjoying 30 days of python"))
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))
+print(type(9.8))        # Float
+print(type(4 - 4j))      # Complex number
+print(type('Matias'))   # String
+print(type(['Asabeneh','Python',"Finland"]))  # List
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(True))   #Boolean 
